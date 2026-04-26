3f1808381477396f52f494c68da0846551441494
floresjr0 <marteflores07@gmail.com>
2026-04-26 15:16:32 +0800

    update button

--- web.php
+++ web.php
@@ -88,7 +88,7 @@
           </button>
 
           <!-- Continue Browsing button -->
-          <button class="btn-primary-lg" id="continueBrowsingBtn" href="index.php">
+          <button class="btn-primary-lg" id="continueBrowsingBtn" onclick="window.location.href='index.php'">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <circle cx="12" cy="12" r="10"/>
               <polyline points="12 8 16 12 12 16"/>
